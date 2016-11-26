bf1c28d648bf4438279d8bd2d81fa135df1ac7f6
kapturyna <wawrzynczak.martyna@gmail.com>
2016-11-26 23:29:56 +0100

    dodanie przypadków testowych

--- src/main/java/Palindromy.java
+++ src/main/java/Palindromy.java
@@ -2,7 +2,7 @@
 public class Palindromy {
 
     public static boolean jestPalindromem(String napis) {
-        return new StringBuilder(napis).reverse().toString().equals(napis);
+        return new StringBuilder(napis).reverse().toString().equalsIgnoreCase(napis);
     }
 
 }
--- src/test/java/PalindromyTest.java
+++ src/test/java/PalindromyTest.java
@@ -4,11 +4,24 @@ import org.junit.Test;
 public class PalindromyTest {
 
     @Test
-    public void testJestPalindromem() {
-        Assert.assertFalse(Palindromy.jestPalindromem("abc"));
+    public void jestPalindromemZwracaTrueDlaNapisuBedacegoPalindromem() {
         Assert.assertTrue(Palindromy.jestPalindromem("kajak"));
-        Assert.assertTrue(Palindromy.jestPalindromem("ala"));
-        Assert.assertEquals(true, Palindromy.jestPalindromem("potop"));
+        Assert.assertTrue(Palindromy.jestPalindromem("Potop"));
+    }
+
+    @Test
+    public void jestPalindromemZwracaFalseDlaNapisuNieBedacegoPalindromem() {
+        Assert.assertFalse(Palindromy.jestPalindromem("abc"));
+    }
+
+    @Test
+    public void jestPalindromemZwracaTrueDlaPustegoNapisu() {
+        Assert.assertTrue(Palindromy.jestPalindromem(""));
+    }
+
+    @Test
+    public void jestPalindromemZwracaTrueDlaNapisuSkladajacegoSieZBialychZnakow() {
+        Assert.assertTrue(Palindromy.jestPalindromem("             "));
     }
 
 }
